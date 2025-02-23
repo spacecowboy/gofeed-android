a5db19934fc3f3ea68ecf38cc4f51c51f5bc6bce
Jonas Kalderstam <spacecowboy@users.noreply.github.com>
2025-02-23 21:23:37 +0100

    chore: fix release-please version marker (#19)
    * chore: fix release-please version marker
    
    * chore: use simple release type
    
    Signed-off-by: Jonas Kalderstam <jonas@cowboyprogrammer.org>
    
    * hmm
    
    Signed-off-by: Jonas Kalderstam <jonas@cowboyprogrammer.org>
    
    ---------
    
    Signed-off-by: Jonas Kalderstam <jonas@cowboyprogrammer.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 group = "com.nononsenseapps.gofeed"
-// {x-version-start:.:<current>}
-version = "0.1.3-SNAPSHOT"
-// {x-version-end}
+// x-release-please-start-version
+version = "0.1.2"
+// x-release-please-end-version
