16adbf44ae71d14b9cbacafa3f90f34b74f766d5
Jonas Kalderstam <spacecowboy@users.noreply.github.com>
2026-08-20 09:12:56 +0200

    fix: added workaround for JSON parsing bug (#76)
    Added workaround for https://github.com/mmcdole/gofeed/issues/344
    
    Signed-off-by: Jonas Kalderstam <jonas@cowboyprogrammer.org>

--- gofeed/export.go
+++ gofeed/export.go
@@ -1,40 +1,9 @@
 package gofeedandroid
 
-import (
-	"bytes"
-	"encoding/json"
-
-	"github.com/mmcdole/gofeed"
-)
-
 func ParseBodyString(body string) ([]byte, error) {
-	fp := gofeed.NewParser()
-	// Use custom translator to work around an image quirk
-	fp.RSSTranslator = NewCustomRssTranslator()
-
-	feed, err := fp.ParseString(body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	b, err := json.Marshal(feed)
-
-	return b, err
+	return ParseBodyBytes([]byte(body))
 }
 
 func ParseBodyBytes(body []byte) ([]byte, error) {
-	fp := gofeed.NewParser()
-	// Use custom translator to work around an image quirk
-	fp.RSSTranslator = NewCustomRssTranslator()
-
-	feed, err := fp.Parse(bytes.NewReader(body))
-
-	if err != nil {
-		return nil, err
-	}
-
-	b, err := json.Marshal(feed)
-
-	return b, err
+	return parseBytes(body)
 }
--- gofeed/go.mod
+++ gofeed/go.mod
@@ -6,14 +6,18 @@ toolchain go1.26.5
 
 require (
 	github.com/mmcdole/gofeed v1.4.1
+	github.com/stretchr/testify v1.11.1
 	golang.org/x/mobile v0.0.0-20260811163000-1769bbc171f0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mmcdole/goxpp/v2 v2.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/mod v0.38.0 // indirect
 	golang.org/x/net v0.57.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
 	golang.org/x/tools v0.48.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
